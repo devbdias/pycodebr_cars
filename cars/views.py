1618adf582b573ba280a34edcdbf60aa83647786
devbdias <dev.brunodias@gmail.com>
2024-05-22 17:20:15 -0300

    entendendo templates e Django Template Language (DTL)

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from cars.models import Car
 
 def cars_view(request):
-    return HttpResponse("Meus carros!")
+
+    cars = Car.objects.all()
+    for car in cars:
+        print(car.photo)
+        
+    return render(
+        request, 
+        'cars.html', 
+        {'cars': cars}
+    )
