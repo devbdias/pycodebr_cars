26cfc369aab61ff849608322f49996e102d44896
devbdias <dev.brunodias@gmail.com>
2024-05-27 18:27:41 -0300

    feat: adicionando tela de login

--- app/urls.py
+++ app/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from cars.views import cars_view, new_car_view
-from accounts.views import register_view
+from accounts.views import register_view, login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +28,7 @@ urlpatterns = [
     path('', cars_view, name='cars_list'),
     path('cars/', cars_view, name='cars_list'),
     path('new_car/', new_car_view, name='new_car'),
+    path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),
 
     
