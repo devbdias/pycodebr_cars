08d7affb5128f137774e45c8781cc33559962462
devbdias <dev.brunodias@gmail.com>
2024-05-27 06:19:35 -0300

    feat: crianto tela de registro

--- app/urls.py
+++ app/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from cars.views import cars_view, new_car_view
+from accounts.views import register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +28,7 @@ urlpatterns = [
     path('', cars_view, name='cars_list'),
     path('cars/', cars_view, name='cars_list'),
     path('new_car/', new_car_view, name='new_car'),
+    path('register/', register_view, name='register'),
 
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
